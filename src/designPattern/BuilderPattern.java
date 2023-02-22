d5b1646959eeacf48efc42f2654da4f5053faa5a
startfrombt <82436530+startfrombt@users.noreply.github.com>
2023-02-22 23:46:29 +0900

    Effective Java의 Builder Pattern 과 테스트

--- src/designPattern/BuilderPattern.java
+++ src/designPattern/BuilderPattern.java
@@ -1,5 +1,89 @@
 package designPattern;
 
-public class BuilderPattern {
+import java.util.Objects;
 
+//Effective Java의 Builder Pattern
+public class BuilderPattern {
+	private final String id;
+    private final String pwd;
+    private final int age;
+    private final String email;
+    
+ // 반드시 Builder 를 사용하여 객체를 생성하도록 하기 위해 private
+    private BuilderPattern(Builder builder) {
+        this.id = builder.id;
+        this.pwd = builder.pwd;
+        this.age = builder.age;
+        this.email = builder.email;
+    }
+    
+    public String getId() {
+		return id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public static class Builder {
+        // Required parameters(필수 인자)
+        private String id;
+        private String pwd;
+
+        // Optional parameters - initialized to default values(선택적 인자는 기본값으로 초기화)
+        private int age = 0;
+        private String email = "";
+
+        public Builder(String id, String pwd) {
+            this.id = id;
+            this.pwd = pwd;
+        }
+
+        public Builder id(String id) {
+            this.id = id;
+            return this; // 이렇게 하면 . 으로 체인을 이어갈 수 있다.
+        }
+
+        public Builder pwd(String pwd) {
+            this.pwd = pwd;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public BuilderPattern build() {
+            return new BuilderPattern(this);
+        }
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BuilderPattern user = (BuilderPattern) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
+
--- src/test/BuilderPatternTest.java
+++ src/test/BuilderPatternTest.java
@@ -1,14 +1,32 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import designPattern.BuilderPattern;
 
 public class BuilderPatternTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void builderTest() {
+		BuilderPattern bp1 = new BuilderPattern.Builder("홍길동", "1234").build();
+		BuilderPattern bp2 = new BuilderPattern.Builder("홍길동", "1234").build();
+		
+		// eqauls() 는 id 값만 비교하고 있음
+		assertTrue(bp1.equals(bp2));
+		
+		BuilderPattern bp3 = new BuilderPattern.Builder("홍길동", "1234")
+				.age(28)
+				.email("naver.com")
+				.build();
+		
+		BuilderPattern bp4 = new BuilderPattern.Builder("홍길동", "1234")
+				.age(30)
+				.email("daum.net")
+				.build();
+		
+		assertTrue(bp3.getAge() != bp4.getAge());
 	}
 
 }
